e8c869aac7c776d967a117f40b1e2e1a06e45de7
Kensaku Nagauchi <kensaku.nagauchi@access-company.com>
2020-04-03 20:28:02 +0900

    [模範回答] Kotlin introduction Exersize(1) Fizz Buzz

--- app/src/test/java/io/access/kotlintraining/SampleTest.kt
+++ app/src/test/java/io/access/kotlintraining/SampleTest.kt
@@ -18,4 +18,25 @@ class SampleTest {
     fun testHello() {
         println("Hello world!")
     }
+
+    private fun fizzBuzz(number: Int): String {
+        return when {
+            number % 15 == 0 -> "FizzBuzz"
+            number % 5 == 0 -> "Buzz"
+            number % 3 == 0 -> "Fizz"
+            else -> "$number"
+        }
+    }
+
+    private fun fizzBuzz2(number: Int): String {
+         val output = "${if (number % 3 == 0) "Fizz" else ""}${if (number % 5 == 0) "Buzz" else ""}"
+         return if (output.isBlank()) "$number" else output
+    }
+
+    @Test
+    fun testFizzBuzz() {
+        for (i in 1..100) {
+            assertEquals(expected = fizzBuzz(i), actual = fizzBuzz2(i))
+        }
+    }
 }
